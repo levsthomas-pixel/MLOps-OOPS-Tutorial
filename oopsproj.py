54f3cb54fee383ce3b3b600db72f3aacf0317186
Bot <levs.thomas@gmail.com>
2025-09-17 21:45:35 +0530

    Sign up and sign in added

--- oopsproj.py
+++ oopsproj.py
@@ -14,14 +14,44 @@ class chatbook:
         "5. Press any other key to exit ")
 
         if user_input == "1":
-            pass
+            self.signup()
         if user_input == "2":
-            pass
+            self.signin()
         if user_input == "3":
             pass
         if user_input == "4":
             pass
         else:
             exit()
+
+    def signup(self):
+        email = input("Set up your email : ")
+        pwd = input("Set up your password : ")
+        self.username = email
+        self.password = pwd 
+        print("You have signed up succesfully!!")
+        print("\n")
+        self.menu()
+
+    def signin(self):
+        if self.username=="" and self.password=="":
+            print("Please signin by pressing 1 in menu")
+        else:
+            uname = input("Enter your email : ")
+            pwd1 = input("Enter your password : ")
+            if self.username==uname and self.password==pwd1:
+                print("You have signed in succesfully!!")
+                self.loggedin = True
+            else:
+                print("Please input correct credentials\n")
+                self.signin()
+
+        print("\n")
+        self.menu()
+
+        
+
+        
+
 
 obj = chatbook()
